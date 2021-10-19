2eae9679513fe0731d2ae333f2118b9ee706c1d3
Usman Siddiqui <e0412913@u.nus.edu>
2021-10-19 19:46:46 +0800

    Update create_customer_table.sql

--- create_customer_table.sql
+++ create_customer_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE Customer(
     password VARCHAR(55) NOT NULL,
     username VARCHAR(55) NOT NULL,
     address VARCHAR(55) NULL,
-    phone_number INT NOT NULL
+    phone_number VARCHAR(55) NOT NULL
     PRIMARY KEY (Customer_id),
     UNIQUE (username),
 );
