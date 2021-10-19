37dcc43a1d35f7c27b5b1c697229121919bc36bb
Usman Siddiqui <e0412913@u.nus.edu>
2021-10-19 19:45:28 +0800

    CREATE INSERT_Customer

---
+++ INSERT_Customer.sql
@@ -0,0 +1,13 @@
+INSERT INTO Customer
+    (Customer_id, email, password, username, address, phone_number)
+VALUES 
+(1,'fbenzing0@umich.edu','Qwerty','mknoble0','8721 Nelson Alley','9386816520');
+-- (2,qgurton1@usa.gov,DVMgir,cmactrusty1,24 Donald Road,9871270500),
+-- (3,bmacartney2@reuters.com,qwerty1234,vbarling2,99881 Thackeray Park,7868126927),
+-- (4,kcoke3@google.com,QWERTY,mjentet3,56 Eastlawn Terrace,7215806703),
+-- (5,ashrive4@usda.gov,JjdLHYaK2R,pkuban4,3732 Judy Crossing,8165373085),
+-- (6,qgurton1@usa.gov,passw0rd1!,imurch5,5 Hagan Road,4693242628),
+-- (7,bperris6@storify.com,uxo4pLe,kantalffy6,7 Dryden Parkway,7187183179),
+-- (8,shelin7@ow.ly,passw0rd1!,slocker7,4737 Tony Center,9882829541),
+-- (9,kbeal8@illinois.edu,Qwerty,phardage8,8721 Nelson Alley,1901109751),
+-- (10,qgurton1@usa.gov,d4xaq8,mstobart9,2107 Utah Court,9737584200)
