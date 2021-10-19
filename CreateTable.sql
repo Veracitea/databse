b68f73df25f2ad550afe8867a9a736f30b49d536
Veracitea <58912917+Veracitea@users.noreply.github.com>
2021-10-19 19:05:59 +0800

    Update CreateTable.sql

--- CreateTable.sql
+++ CreateTable.sql
@@ -9,3 +9,15 @@ CREATE TABLE ContactPerson(
      FOREIGN KEY (companyID) REFERENCES Company(companyID)
      ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+INSERT INTO CheckInOut
+   (UserID,LocationID,CheckinTime,CheckoutTime)
+VALUES 
+('user_1', 'loc_1',1000,2000),
+('user_2', 'loc_2',2000,3000), 
+('user_3', 'loc_3',3000,4000),
+('user_4', 'loc_4',4000,5000),
+('user_5', 'loc_6',5000,6000),
+('user_6', 'loc_6',6000,7000),
+('user_7', 'loc_7',7000,8000)
