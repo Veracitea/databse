3720f5fa8dc2a903b03f91d341ffe68f37d4080a
Tang Han Kang <58020827+tanghankang@users.noreply.github.com>
2021-10-22 01:24:46 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 --Queries:
 --Q1:Given a customer by an email address, returns the product ids that have been ordered
 --and paid by this customer but not yet shipped. (MJ)
-
+SELECT Product_id
+FROM Order_item oi, Invoice i, Orders o, Customer c
+WHERE i.status = 'paid' AND oi.Order_id = i.Order_id
+AND NOT oi.status = 'shipped'
+AND oi.Order_id = o.Order_id
+AND o.Customer_id = c.Customer_id
+AND c.email = 'bmacartney2@reuters.com';
 
 --Q2:Find the 3 bestselling product type ids in terms of product quantity sold. The products of
 --concerned must be ordered and paid. Whether they have been shipped is irrelevant. (BJ)
