4233190c99725ab62fd7e6baec745dd91787b989
Veracitea <58912917+Veracitea@users.noreply.github.com>
2021-10-21 23:20:48 +0800

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,26 +1,33 @@
-Constraints
- When the full payment to an invoice is made, the invoice status is changed from ‘issued’
-to ‘paid’.
+--Constraints
+-- When the full payment to an invoice is made, the invoice status is changed from ‘issued’
+--to ‘paid’.
 
 
- When an order item is shipped, its status is changed from ‘processing’ to ‘shipped’.
+-- When an order item is shipped, its status is changed from ‘processing’ to ‘shipped’.
 
 
- When all the products in an order have been shipped, the order status is changed from
-‘processing’ to ‘completed’.
+-- When all the products in an order have been shipped, the order status is changed from
+--‘processing’ to ‘completed’.
 
 
- There can be at most 3 payments to an invoice, i.e., if the customer chooses to perform
-partial payments, the 3rd payment must complete the full amount.
+-- There can be at most 3 payments to an invoice, i.e., if the customer chooses to perform
+--partial payments, the 3rd payment must complete the full amount.
 
 
- If an ordered has been paid, either fully or partially, it can no longer be cancelled, i.e., its
-status cannot be changed to ‘cancelled’.
-CREATE TRIGGER autoAddMutualFriend ON Friends
-AFTER INSERT
+-- If an ordered has been paid, either fully or partially, it can no longer be cancelled, i.e., its
+--status cannot be changed to ‘cancelled’.
+
+--T ATTEMPT NOT CONFIRMED TO BE RIGHT
+CREATE TRIGGER cancelOrder
+AFTER UPDATE status ON Orders
+REFERENCING OLD TABLE AS Ord
 AS
 BEGIN
-INSERT INTO Friends
-SELECT friendID, ownerID
-FROM INSERTED
+  IF EXIST(
+    SELECT * FROM Payment NATURAL JOIN Invoice AS newT
+    WHERE newT.Invoice_number=Ord.Order_id)
+  BEGIN
+    RAISEERROR('Cannot cancel, payment was made')
+    ROLLBACK TRANSACTION
+  END
 END
