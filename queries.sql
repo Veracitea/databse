722bf5c2fa4ce2c58903d825c9ce0dd9e15ddd25
Veracitea <58912917+Veracitea@users.noreply.github.com>
2021-10-22 00:55:25 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,24 +1,24 @@
 --Queries:
 --Q1:Given a customer by an email address, returns the product ids that have been ordered
---and paid by this customer but not yet shipped.
+--and paid by this customer but not yet shipped. (MJ)
 
 
 --Q2:Find the 3 bestselling product type ids in terms of product quantity sold. The products of
---concerned must be ordered and paid. Whether they have been shipped is irrelevant.
+--concerned must be ordered and paid. Whether they have been shipped is irrelevant. (BJ)
 
 
 --Q3:Return the descriptions of all the 2nd level product types. The product types with no parent
 --will be regarded as 1st level product types and their direct child product types will be
---regarded as 2nd level.
+--regarded as 2nd level. (HK TRY)
 
 
---Q4:Find 2 product ids that are ordered together the most.
+--Q4:Find 2 product ids that are ordered together the most. (T)
 
 
---Q5:Get 3 random customers and return their email addresses.
+--Q5:Get 3 random customers and return their email addresses. (HAHAHA SIKEEEE)
 
 
 --Design two queries that are not in the above list. They are evaluated based on the usefulness,
 --complexity, and the interestingness.
--Q6:
--Q7:
+--Q6:ASDFGHJKL;'\
+--Q7:
