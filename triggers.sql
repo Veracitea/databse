0d481729e0dc84aa01b3bb317b8d66d1673c88db
Tang Han Kang <58020827+tanghankang@users.noreply.github.com>
2021-10-22 01:02:01 +0800

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -16,8 +16,32 @@
 
 -- If an ordered has been paid, either fully or partially, it can no longer be cancelled, i.e., its
 --status cannot be changed to ‘cancelled’.
+--HK attempt #2
 
---HK attempt
+CREATE TRIGGER cancelOrder
+ON Orders
+AFTER UPDATE
+AS
+BEGIN
+  IF EXIST(
+    SELECT * 
+    FROM Payment NATURAL JOIN Invoice AS newT, inserted AS i, deleted AS d
+    WHERE i.status = 'cancelled'
+    AND (d.status = 'processing' OR d.status = 'completed')
+    AND newT.Order_id=i.Order_id;)
+  BEGIN
+    RAISEERROR('Cannot cancel, payment was made',16,1)
+    ROLLBACK TRANSACTION
+    RETURN
+  END
+  UPDATE Order_item
+  SET status = 'cancelled'
+  FROM inserted AS i
+  WHERE Order_id = i.Order_id;
+END
+
+
+--HK attempt #1
 CREATE TRIGGER cancelOrder
 AFTER UPDATE status ON Orders
 REFERENCING OLD TABLE AS Ord
