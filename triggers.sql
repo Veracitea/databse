ee2aea5a48741bf2e5d7aad787e77b9f2484b88a
Tang Han Kang <58020827+tanghankang@users.noreply.github.com>
2021-10-22 23:55:06 +0800

    Create triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -16,46 +16,33 @@
 
 -- If an ordered has been paid, either fully or partially, it can no longer be cancelled, i.e., its
 --status cannot be changed to ‘cancelled’.
---T attempt #2
-CREATE TRIGGER cancelOrder
-ON Orders 
-AFTER UPDATE 
-AS  
-IF EXISTS (SELECT 1 
-    FROM Payment AS p, Invoice AS inv , inserted AS i, deleted AS d
-    WHERE i.status = 'cancelled'
-    AND (d.status = 'processing' OR d.status = 'completed')
-    AND inv.Order_id=i.Order_id AND p.Invoice_number=inv.Invoice_number
-          )  
-BEGIN  
-RAISERROR ('A vendor''s credit rating is too low to accept new purchase orders.', 16, 2);  
-ROLLBACK TRANSACTION
-RETURN   
-END;  
-
 --HK attempt #2
 
 CREATE TRIGGER cancelOrder
 ON Orders
 AFTER UPDATE
 AS
+IF inserted.status <> 'cancelled'
+RETURN;
 BEGIN
-  IF EXIST(
+  IF EXISTS(
     SELECT * 
-    FROM Payment NATURAL JOIN Invoice AS newT, inserted AS i, deleted AS d
-    WHERE i.status = 'cancelled'
-    AND (d.status = 'processing' OR d.status = 'completed')
-    AND newT.Order_id=i.Order_id;)
+    FROM Payment as p JOIN Invoice AS inv ON p.Invoice_number = inv.Invoice_number, inserted AS i, deleted AS d
+    WHERE d.status <> 'cancelled'
+    AND inv.Order_id=i.Order_id)
   BEGIN
     RAISEERROR('Cannot cancel, payment was made',16,1)
     ROLLBACK TRANSACTION
     RETURN
   END
+  ELSE
+  BEGIN
   UPDATE Order_item
   SET status = 'cancelled'
   FROM inserted AS i
   WHERE Order_id = i.Order_id;
-END
+  END;
+END;
 
 
 --HK attempt #1
